4c452a209276cc3ce5e6983ec85e674d737a6180
박종장 <pjjcmr@gmail.com>
2026-01-23 11:51:49 +0900

    26-3

--- Notes/COMAZON/src/utils/graceful-shutdown.util.js
+++ Notes/COMAZON/src/utils/graceful-shutdown.util.js
@@ -10,45 +10,25 @@ export const setupGracefulShutdown = (server, prisma) => {
       await new Promise((resolve, reject) => {
         server.close((err) => {
           if (err) return reject(err);
-<<<<<<< HEAD
           console.log('서버가 종료되었습니다.');
-=======
-          console.log("서버가 종료되었습니다.");
->>>>>>> ffbc5cb86181e0a736384d59411afe611f1bc284
           resolve();
         });
       });
 
       // 2. Prisma 연결 종료 (Pool 포함)
       await prisma.$disconnect();
-<<<<<<< HEAD
       console.log('데이터베이스 연결이 종료되었습니다.');
 
       process.exit(0);
     } catch (error) {
       console.error('종료 중 에러:', error);
-=======
-      console.log("데이터베이스 연결이 종료되었습니다.");
-
-      process.exit(0);
-    } catch (error) {
-      console.error("종료 중 에러:", error);
->>>>>>> ffbc5cb86181e0a736384d59411afe611f1bc284
       process.exit(1);
     }
   };
 
   // SIGINT: Ctrl+C
-<<<<<<< HEAD
   process.on('SIGINT', () => shutdown('SIGINT'));
 
   // SIGTERM: 클라우드 플랫폼의 종료 신호
   process.on('SIGTERM', () => shutdown('SIGTERM'));
 };
-=======
-  process.on("SIGINT", () => shutdown("SIGINT"));
-
-  // SIGTERM: 클라우드 플랫폼의 종료 신호
-  process.on("SIGTERM", () => shutdown("SIGTERM"));
-};
->>>>>>> ffbc5cb86181e0a736384d59411afe611f1bc284
--- Notes/COMAZON/src/utils/index.js
+++ Notes/COMAZON/src/utils/index.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 export { hashPassword, comparePassword } from './hash.util.js';
 export { setAuthCookies } from './cookie.util.js';
-=======
-export { hashPassword, comparePassword } from "./hash.util.js";
-export { setAuthCookies } from "./cookie.util.js";
->>>>>>> ffbc5cb86181e0a736384d59411afe611f1bc284
 export {
   generateTokens,
   generateAccessToken,
   generateRefreshToken,
   verifyToken,
-<<<<<<< HEAD
-  // shouldRefreshToken,
-  // refreshTokens,
+  //shouldRefreshToken,
+  //refreshTokens,   // 나중에 추가 jwt.util.js에 반영될 경우
 } from './jwt.util.js';
 export { setupGracefulShutdown } from './graceful-shutdown.util.js';
-=======
-  shouldRefreshToken,
-  refreshTokens,
-} from "./jwt.util.js";
-export { setupGracefulShutdown } from "./graceful-shutdown.util.js";
->>>>>>> ffbc5cb86181e0a736384d59411afe611f1bc284
